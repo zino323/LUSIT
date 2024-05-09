2d96d643084f1da9b25d49b6a30258ebfe7d6283
zinen33 <156739195+zinen33@users.noreply.github.com>
2024-05-09 15:45:52 +0100

    Update ابتايم.js

--- script/ابتايم.js
+++ script/ابتايم.js
@@ -1,32 +1,49 @@
 module.exports.config = {
-  name: "ابتايم",
-  version: "1.0.0",
-  role: 0,
-  credits: "يـاسـيـن",
-  description: "خدمات",
-  usePrefix: false,
-  commandCategory: "other",
-  cooldowns: 5,
-	dependencies: {
-		"pidusage": ""
-	}
+
+    name: "وقت",
+
+    version: "1.0.1", 
+
+    role: 0,
+
+    credits: "joshua", 
+
+    description: "عرض وقت تشغيل البوت.",
+
+    usePrefix: false,
+
+    commandCategory: "...",
+
+    cooldowns: 1,
+
+    dependencies: {
+
+        "request":"",
+
+        "fs-extra":"",
+
+        "axios":""
+
+    }
+
 };
 
-function byte2mb(bytes) {
-	const units = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-	let l = 0, n = parseInt(bytes, 10) || 0;
-	while (n >= 1024 && ++l) n = n / 1024;
-	return `${n.toFixed(n < 10 && l > 0 ? 1 : 0)} ${units[l]}`;
-}
+module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
 
-module.exports.run = async ({ api, event }) => {
-	const time = process.uptime(),
-		hours = Math.floor(time / (60 * 60)),
-		minutes = Math.floor((time % (60 * 60)) / 60),
-		seconds = Math.floor(time % 60);
+    const time = process.uptime(),
 
-	const pidusage = await global.nodemodule["pidusage"](process.pid);
+        hours = Math.floor(time / (60 * 60)),
 
-	const timeStart = Date.now();
-	return api.sendMessage("", event.threadID, () => api.sendMessage(`وقـت الـتـشـغـيـل ${hours}  سـاعـة و ${minutes} دقـيـقـة و ${seconds} ثـانـيـة.\n\n✅❯ عـدد الـمـسـتـخـدمـيـن: 【${global.data.allUserID.length}】\n\n✅❯ عـدد الـمـجـمـوعـات: 【${global.data.allThreadID.length}】\n\n✅❯ اسـتـخـدام الـمـعـالـج: 【${pidusage.cpu.toFixed(1)}】%\n\n✅❯ اسـتـخـدام الـرام: 【${byte2mb(pidusage.memory)}】\n\n✅❯ الـبـيـنـج: 【${Date.now() - timeStart}】 مـلـي ثـانـيـة`, event.threadID, event.messageID));
-}
+        minutes = Math.floor((time % (60 * 60)) / 60),
+
+        seconds = Math.floor(time % 60);
+
+    const moment = require("moment-timezone");
+
+    var juswa = moment.tz("Europe/Britain").format("『D/MM/YYYY』 【HH:mm:ss】");
+
+    return api.sendMessage(`== معلومات وقت التشغيل   ==\n\nالساعات:${hours}\n الدقائق:${minutes}\n الثواني:${seconds}\n
+
+━━━━━━━━━━━━━━`, event.threadID);
+
+};
